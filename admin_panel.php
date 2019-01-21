4e4a5494971ce6cc5d8725af1b2ea29312756f40
Chandan Shastri <crshastri@gmail.com>
2019-01-21 16:08:47 +0530

    Fixed Fonts.

--- admin_panel.php
+++ admin_panel.php
@@ -21,7 +21,7 @@
       <div class="collapse navbar-collapse" id="navbarNav">
     <ul class="navbar-nav">
       <li class="nav-item active">
-        <a class="nav-link" href="index.php">Placement Dept..<span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="index.php">Placement Dept<span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="admin.php">Logout</a>
